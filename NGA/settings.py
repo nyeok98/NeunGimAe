e819a6a73d305f5b865656cfdca3830712a1cebe
kdh4387 <kdh4387@naver.com>
2020-09-07 19:13:27 +0900

    new static path

--- NGA/settings.py
+++ NGA/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, "/account/static")
+    os.path.join(BASE_DIR, "/account/static"),
+    os.path.join(BASE_DIR, "/mypage/static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
