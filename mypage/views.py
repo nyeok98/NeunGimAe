7be4a67b941a97110ddeb13ca6da0cb5b82a0914
bogyeong <bkkim2897@gmail.com>
2020-10-28 18:06:06 +0900

    timer

--- mypage/views.py
+++ mypage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import CustomUserChangeForm
 from account.models import Account
 from main.models import Blog
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -16,7 +17,7 @@ def profile_update(request):
     if request.method == 'POST':
         user_change_form = CustomUserChangeForm(request.POST, instance=request.user)
         if user_change_form.is_valid():
-            user_change_form.save()        
+            user_change_form.save() 
             return redirect('mypage')
 
     else:
@@ -25,11 +26,12 @@ def profile_update(request):
 
 def password(request):
     if request.method == 'POST':
-        password_change_form = PasswordChangeForm(request.POST, request.user)
+        password_change_form = PasswordChangeForm(request.user, request.POST)
+        
         if password_change_form.is_valid():
-            password_change_form.save()
+            user = password_change_form.save()
+            update_session_auth_hash(request, user)
             return redirect('mypage')
-    
     else:
         password_change_form = PasswordChangeForm(request.user)
-    return render(request, 'password.html',{'password_change_form':password_change_form})
+    return render(request, 'password.html',{ 'password_change_form': password_change_form} )
